2859abb2cfdfab50212e1a6ecb664a37e37535ef
Martin Corbau <martincorbau@hotmail.com>
2019-06-19 15:45:22 +0200

    Learning about built in methods in ruby

--- Learning-ruby/Hello_World.rb
+++ Learning-ruby/Hello_World.rb
@@ -2,9 +2,12 @@
 #puts "Hello World!"
 
 def say_hello(name)
-  puts "Hello "+name
+  puts "Hello #{name}"
 end
 prenom="Martin"
 nom="Corbau"
 say_hello("Martin")
 puts "#{prenom} #{nom}"
+
+puts prenom.empty?
+puts prenom.reverse
