1f40521c4f455a42133acdcf63d0cd9484e8b3da
Martin Corbau <martincorbau@hotmail.com>
2019-06-20 10:31:21 +0200

    Using Branching to let the user choose the command to do

--- Learning-ruby/Simple-Calc.rb
+++ Learning-ruby/Simple-Calc.rb
@@ -25,8 +25,19 @@ puts "Enter the first Number:"
 num1 = gets.chomp
 puts "Enter the second Number:"
 num2 = gets.chomp
-puts "The first number multiplied by the second is #{multiply(num1,num2)}"
-puts "The first number divided by the second is #{divide(num1,num2)}"
-puts "The first number plus the second is #{addition(num1,num2)}"
-puts "The first number minus the second is #{minus(num1,num2)}"
-puts "The modulus of the first number by the second is #{modulus(num1,num2)}"
+puts "What du you want to do?"
+puts "Type 1 for Addition, 2 for Subtraction, 3 for division, 4 for Multliplication and 5 for the modulus"
+entry = gets.chomp
+if entry == "1"
+  puts "The first number plus the second is #{addition(num1,num2)}"
+elsif entry == "2"
+  puts "The first number minus the second is #{minus(num1,num2)}"
+elsif entry == "3"
+  puts "The first number divided by the second is #{divide(num1,num2)}"
+elsif entry == "4"
+  puts "The first number multiplied by the second is #{multiply(num1,num2)}"
+elsif entry == "5"
+  puts "The modulus of the first number by the second is #{modulus(num1,num2)}"
+else
+  puts "not a given command"
+end
